180ab7bb0d33c8b0c333e9d9957ea787a9c90fec
sbalayan1 <balayans2014@yahoo.com>
2023-04-07 16:31:00 -0700

    adds a Form component a new way to create a functional component

---
+++ src/Form.tsx
@@ -0,0 +1,27 @@
+
+//essentially does the same but instead types our function the FunctionComponent type built into react. This lets us set the component to a specific type which then affects the available props
+import React, {FC} from 'react'
+
+
+
+interface FakeProps {
+    readonly id: number
+    name: string
+
+
+}
+
+interface MoreProps {
+    phoneNumber: number
+}
+
+const MyForm: FC<FakeProps | MoreProps> = (props) => {
+    return (
+        <form>
+             <input>{"phoneNumber" in props ? props.phoneNumber : props.name}</input>
+            <input></input>
+        </form>
+    )
+}
+
+export default MyForm
